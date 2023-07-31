92b3996a472d5d668edd198e5be36081763b7e98
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2023-07-31 19:03:41 +0500

    update

---
+++ IdentityCoreSetup/Controllers/RentController.cs
@@ -0,0 +1,49 @@
+﻿using IdentityCoreSetup.Entities;
+using IdentityCoreSetup.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCoreSetup.Controllers
+{
+    public class RentController : Controller
+    {
+        RentServices RentServices = new RentServices();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult RentIndex()
+        {
+            var data = RentServices.GetRents();
+            return new JsonResult(data);
+        }
+
+        [HttpPost]
+        public JsonResult Create(Rent Rent)
+        {
+            RentServices.CreateRent(Rent);
+            return new JsonResult("Rent Added");
+
+        }
+
+        public JsonResult Delete(int id)
+        {
+            RentServices.DeleteRent(id);
+            return new JsonResult("Rent Removed");
+        }
+
+        public JsonResult Edit(int id)
+        {
+            var data = RentServices.GetRentById(id);
+            return new JsonResult(data);
+        }
+        [HttpPost]
+        public JsonResult Update(Rent Rent)
+        {
+            RentServices.UpdateRent(Rent);
+            return new JsonResult("Record Updated");
+        }
+    }
+}
---
+++ IdentityCoreSetup/Services/RentServices.cs
@@ -0,0 +1,57 @@
+﻿using IdentityCoreSetup.Data;
+using IdentityCoreSetup.Entities;
+
+namespace IdentityCoreSetup.Services
+{
+    public class RentServices
+    {
+       
+        public List<Rent> GetRents()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var data = context.rents.ToList();
+                data.Reverse();
+                return data;
+            }
+        }
+
+
+        public Entities.Rent GetRentById(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.rents.Find(id);
+
+            }
+        }
+
+        public void CreateRent(Rent Rent)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.rents.Add(Rent);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateRent(Rent Rent)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.rents.Update(Rent);
+                context.SaveChanges();
+            }
+        }
+        public void DeleteRent(int id)
+        {
+            var data = GetRentById(id);
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.rents.Remove(data);
+                context.SaveChanges();
+            }
+        }
+    }
+}
